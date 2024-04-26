6791931038a9a9db90348c9b8cd9968977e42ab3
Letsdo123 <108178573+Letsdo123@users.noreply.github.com>
2024-04-26 12:49:59 +0000

    some inistial commit and push

--- megaBlogAppwrite/src/App.jsx
+++ megaBlogAppwrite/src/App.jsx
@@ -1,50 +1,38 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { authService } from './appwrite/auth'
-import { login, logout } from './store/authSlice'
-import {Header,Footer} from './components'
+import './App.css'
+import {authService} from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
+
 function App() {
-  // For accessing the env variable in vite application
-  // console.log(config.appwriteUrl);
   const [loading, setLoading] = useState(true)
   const dispatch = useDispatch()
+
   useEffect(() => {
     authService.getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({
-            userData: userData
-          }))
-        }
-        else {
-          dispatch(logout())
-        }
-      })
-      .catch((error) => {
-        console.log("Error in authentication :: error", error);
-      })
-      .finally(() => {
-        setLoading(false)
-      })
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
   }, [])
+  
   return !loading ? (
-    <>
-      <h2>Blog app with appwrite</h2>
-      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-        <div className="w-full block">
-          <Header/>
-          <main>
-            {/* Outlet will be updated here */}
-          </main>
-          <Footer/>
-        </div>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
       </div>
-    </>
-  ) : 
-  (<>
-    <p>You didn't login yet</p>
-  </>)
+    </div>
+  ) : null
 }
 
 export default App
